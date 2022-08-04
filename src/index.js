19a4222641c1b3d00b0545fc1a26ebcc799f711d
NadzeyaShu <chunta89@gmail.com>
2022-08-04 15:05:54 +0400

    feat: implementation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+    if (!matrix) {
+        return [];
+    }
+    matrix.forEach((value, index) => {
+        if (index % 2 !== 0) {
+            value.sort((a, b) => b - a);
+        }
+    })
+
+    return matrix.reduce((sumValue, currentValue) => sumValue.concat(currentValue), []);
 }
